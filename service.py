34194263b0ecf82c9b6f1ce65fe5d022d15f6752
Marc Esher <marc.esher@gmail.com>
2017-07-03 14:21:14 -0400

    Make URL configurable for each click type via env variable

--- service.py
+++ service.py
@@ -15,18 +15,18 @@ def handler(event, context):
     click_type = event.get("clickType")
 
     call_to = os.environ['to_number_single_click']
+    url = os.environ['url_single_click']
     if click_type == "DOUBLE":
         call_to = os.environ['to_number_double_click']
+        url = os.environ['url_double_click']
     elif click_type == "LONG":
         call_to = os.environ['to_number_long_click']
+        url = os.environ['url_long_click']
     print "Calling %s" % call_to
 
 
     client = Client(sid, token)
-    call = client.api.account.calls \
-        .create(to=call_to,
-        from_=call_from,
-        url="http://twimlets.com/message?Message%5B0%5D=Hooray%20you%20found%20me&Message%5B1%5D=I%20and%20love%20and%20you")
+    call = client.api.account.calls.create(to=call_to, from_=call_from, url=url)
 
     return True
 
